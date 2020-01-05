24dfb8f4c148dd62a05ad507c2b93578c3c60eca
ARana16 <40147496+ARana16@users.noreply.github.com>
2020-01-05 16:12:20 +0530

    uploading the code

---
+++ src/components/Todo.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import './Todo.css';
+import CreateTask from './CreateTask'
+import Task from './Task'
+import ls from 'local-storage'
+function Todo() {
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [tasks, setTasks] = useState(ls.get('myTasks')
+        /*[
+        {
+            title: "My Morning Task",
+            completed: true
+        },
+        {
+            title: "Breakfast",
+            completed: true
+        },
+        {
+            title: "Go for Movie",
+            completed: false
+        }
+    ]*/
+    );
+
+    useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) });
+
+
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+        ls.set('myTasks',newTasks);
+    };
+
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+        ls.set('myTasks',newTasks);
+    };
+
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+        ls.set('myTasks',newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="header">Pending tasks ({tasksRemaining})</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
